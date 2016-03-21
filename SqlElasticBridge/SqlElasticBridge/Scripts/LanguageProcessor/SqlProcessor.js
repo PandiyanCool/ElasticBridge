534c475cec8c7c33526e210f0caef1160dda7b80
Pandiyan <pandiyanit7@gmail.com>
2016-03-22 00:17:41 +0530

    Added Sql Processor

---
+++ SqlElasticBridge/SqlElasticBridge/Scripts/LanguageProcessor/SqlProcessor.js
@@ -0,0 +1,14 @@
+﻿
+var sqlEditor = ace.edit("sqlEditor");
+sqlEditor.setTheme("ace/theme/solarized_dark");
+sqlEditor.getSession().setMode("ace/mode/sql");
+
+var elasticEditor = ace.edit("elasticEditor");
+elasticEditor.setTheme("ace/theme/solarized_dark");
+elasticEditor.getSession().setMode("ace/mode/json");
+
+function sqlProcessor() {
+	var sqlReader = $('#sqlEditor > div.ace_scroller').text();
+	
+	elasticEditor.insert(sqlReader);
+}
